e205ab3c6f0139a5e814fab09a212e6d5aeb74e0
Taylor Watson <taylorwatsonb@gmail.com>
2018-10-03 02:02:57 +0000

    automatically backed up by learn

---
+++ beatles.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
