f00c995ebf488248c1df493bbe8214eb9195d1f1
Taylor Watson <taylorwatsonb@gmail.com>
2018-10-03 02:05:33 +0000

    automatically backed up by learn

--- beatles.js
+++ beatles.js
@@ -17,3 +17,12 @@ function johnLennonFacts() {
     return newFacts;
 }
 
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+    
+    return love;
+}
